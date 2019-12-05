9737e7ac5824c60fd37f8dc36d905ea64dcb4f43
unknown <culaficstefan@gmail.com>
2019-12-04 19:55:02 -0500

    example

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const router  = express.Router();
+const Movie   = require('../models/movie');
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
+});
+
+router.get('/movies', (req, res, next) => {
+  Movie.find().then(movies => {
+    console.log(movies);
+    res.render('movies', {movies: movies});
+  })
+})
+
+router.get('/movie/:id', (req, res,next)=> {
+  Movie.findById(req.params.id).then(movie => {
+    res.render('movie', {movie: movie});
+  });
 });
 
 module.exports = router;
